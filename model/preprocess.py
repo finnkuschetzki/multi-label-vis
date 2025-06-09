0d33c0161efed339d385b6cb97b06d8f76b14496
Finn Kuschetzki <finnmail@icloud.com>
2025-06-09 15:52:31 +0200

    limited to categories as standard in preprocess - fixing multi_hot_encoding mappings

--- model/preprocess.py
+++ model/preprocess.py
@@ -7,7 +7,7 @@ DATA_DIR = "../coco_dataset/data"
 TRAIN_IMG_DIR = DATA_DIR + "/images/train2017"
 VAL_IMG_DIR = DATA_DIR + "/images/val2017"
 
-CATEGORY_INDICES = [
+CATEGORY_IDS = [
     44, 46, 47, 48, 49, 50, 51,  # kitchen categories
     62, 63, 64, 65, 67, 70,  # furniture categories
     72, 73, 74, 75, 76, 77  # electronic furniture
@@ -20,13 +20,14 @@ print()
 coco_val = COCO(DATA_DIR + "/annotations/instances_val2017.json")
 
 categories = coco_train.loadCats(coco_train.getCatIds())  # this assumes that all classes are present in train and val data
+train_categories = [cat for cat in categories if cat["id"] in CATEGORY_IDS]
 num_coco_classes = len(categories)
-num_train_classes = len(CATEGORY_INDICES)
+num_train_classes = len(CATEGORY_IDS)
 
 # map category_id to multi_hot index
 category_id_to_multi_hot_index = {
     category["id"]: multi_hot_index
-    for multi_hot_index, category in enumerate(categories)
+    for multi_hot_index, category in enumerate(train_categories)
 }
 
 multi_hot_index_to_category_id = {
@@ -36,7 +37,7 @@ multi_hot_index_to_category_id = {
 
 multi_hot_index_to_category_name = dict([
     (category_id_to_multi_hot_index[category["id"]], category["name"])
-    for category in categories
+    for category in train_categories
 ])
 
 # map images to labels
@@ -48,8 +49,9 @@ val_anns = coco_val.loadAnns(coco_val.getAnnIds())
 for ann in train_anns + val_anns:
     image_id = ann["image_id"]
     category_id = ann["category_id"]
-    multi_hot_index = category_id_to_multi_hot_index[category_id]
-    image_labels[image_id].add(multi_hot_index)
+    if category_id in CATEGORY_IDS:
+        multi_hot_index = category_id_to_multi_hot_index[category_id]
+        image_labels[image_id].add(multi_hot_index)
 
 
 # list images with labels
@@ -59,28 +61,16 @@ def get_image_infos(coco_instance, img_dir): # generates image_infos for given c
     for img in coco_instance.loadImgs(coco_instance.getImgIds()):
         img_path = img_dir + "/" + img["file_name"]
         img_labels = image_labels[img["id"]]
-        multi_hot_encoding = np.zeros(num_coco_classes, dtype=np.float32)
+        multi_hot_encoding = np.zeros(num_train_classes, dtype=np.float32)
         multi_hot_encoding[list(img_labels)] = 1.0
-        image_infos.append((img_path, multi_hot_encoding))
+        if not np.all(multi_hot_encoding == 0):
+            image_infos.append((img_path, multi_hot_encoding))
 
     return image_infos
 
 
-# limit labels to specific categories and filter out images without label
-def limit_to_categories(image_infos, category_ids):
-    multi_hot_indices = np.array([category_id_to_multi_hot_index[cat_id] for cat_id in category_ids])
-    filtered_image_infos = []
-
-    for img_path, multi_hot_encoding in image_infos:
-        multi_hot_encoding = multi_hot_encoding[multi_hot_indices]
-        if not np.all(multi_hot_encoding == 0):
-            filtered_image_infos.append((img_path, multi_hot_encoding))
-
-    return filtered_image_infos
-
-
-train_image_infos = limit_to_categories(get_image_infos(coco_train, TRAIN_IMG_DIR), CATEGORY_INDICES)
-val_image_infos = limit_to_categories(get_image_infos(coco_val, VAL_IMG_DIR), CATEGORY_INDICES)
+train_image_infos = get_image_infos(coco_train, TRAIN_IMG_DIR)
+val_image_infos = get_image_infos(coco_val, VAL_IMG_DIR)
 
 
 # configure export
