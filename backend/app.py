6398141e897cefd6ac353ac31065aad0de493bd9
Finn Kuschetzki <finnmail@icloud.com>
2025-06-22 22:05:11 +0200

    added data endpoint in backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,14 @@
+import os
 from flask import Flask
+
+from process_data import *
+
+
+# checking for dimensionality reduction data
+if not os.path.exists("data/dimensionality_reduction.csv"):
+    print()
+    process_data()  # generates and saves dimensionality reduction data
+    print()
 
 app = Flask(__name__)
 
@@ -6,6 +16,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route("/data/")
+def data():
+    return get_data_as_json()
 
 
 if __name__ == '__main__':
--- backend/process_data.py
+++ backend/process_data.py
@@ -12,43 +12,66 @@ WIDTH, HEIGHT = 0.01, 0.01  # results in 1 / (0.01 * 0.01) = 10,000 cells in vis
 DELTA = 1.0
 
 
-# --- read and process data ---
+def process_data():
 
-in_df = pd.read_csv("../model/output/embedding_data.csv")
-in_df["ground_truth"] = in_df["ground_truth"].apply(literal_eval)
-in_df["features"] = in_df["features"].apply(literal_eval)
-in_df["predictions"] = in_df["predictions"].apply(literal_eval)
-in_df["binarized_predictions"] = in_df["binarized_predictions"].apply(literal_eval)
-features = np.array(in_df["features"].tolist())
-
-standard_scaler = StandardScaler()
-standardized_features = standard_scaler.fit_transform(features)
-
-min_max_scaler = MinMaxScaler()
+    print("processing data...")
 
 
-# --- prepare data saving ---
+    # --- read and process data ---
 
-directory = "data"
-if not os.path.exists(directory):
-    os.makedirs(directory)
-    print(f"created directory: {directory}")
+    in_df = pd.read_csv("../model/output/embedding_data.csv")
+    in_df["ground_truth"] = in_df["ground_truth"].apply(literal_eval)
+    in_df["features"] = in_df["features"].apply(literal_eval)
+    in_df["predictions"] = in_df["predictions"].apply(literal_eval)
+    in_df["binarized_predictions"] = in_df["binarized_predictions"].apply(literal_eval)
+    features = np.array(in_df["features"].tolist())
 
-out_df = in_df.filter(["ground_truth", "predictions", "binarized_predictions"])
+    standard_scaler = StandardScaler()
+    standardized_features = standard_scaler.fit_transform(features)
 
-
-# --- PCA ---
-
-pca = PCA(n_components=2)
-pca_features = pca.fit_transform(standardized_features)
-scaled_pca_features = min_max_scaler.fit_transform(pca_features)
-out_df["pca_features"] = scaled_pca_features.tolist()
-
-# overlap removal
-scaled_pca_features_or = DGrid(WIDTH, HEIGHT, DELTA).fit_transform(scaled_pca_features)
-out_df["pca_features_or"] = scaled_pca_features_or.tolist()
+    min_max_scaler = MinMaxScaler()
 
 
-# --- saving data ---
+    # --- prepare data saving ---
 
-out_df.to_csv('data/dimensionality_reduction.csv', index=False)
+    directory = "data"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        print(f"created directory: {directory}")
+
+    out_df = in_df.filter(["ground_truth", "predictions", "binarized_predictions"])
+
+
+    # --- PCA ---
+
+    pca = PCA(n_components=2)
+    pca_features = pca.fit_transform(standardized_features)
+    scaled_pca_features = min_max_scaler.fit_transform(pca_features)
+    out_df["pca_features"] = scaled_pca_features.tolist()
+
+    # overlap removal
+    scaled_pca_features_or = DGrid(WIDTH, HEIGHT, DELTA).fit_transform(scaled_pca_features)
+    out_df["pca_features_or"] = scaled_pca_features_or.tolist()
+
+
+    # --- saving data ---
+
+    out_df.to_csv('data/dimensionality_reduction.csv', index=False)
+
+    print("dimensionality reduction and overlap removal done!")
+
+
+def get_data_as_json():
+
+    df = pd.read_csv("data/dimensionality_reduction.csv")
+    return df.to_json(orient="records")
+
+
+__all__ = [
+    "process_data",
+    "get_data_as_json"
+]
+
+
+if __name__ == "__main__":
+    process_data()
