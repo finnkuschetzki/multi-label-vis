60384a3111442e5635f2e69e7f78732769caa6f9
Finn Kuschetzki <finnmail@icloud.com>
2025-06-30 20:08:13 +0200

    improved tool menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,13 @@ import { ref } from "vue"
 
 // menu refs
 const useDGrid = ref(true)
+
+const dimReductionOptions = ref([
+  { name: "PCA", value: "pca" },
+  { name: "UMAP", value: "umap" },
+  { name: "t-SNE", value: "tsne" }
+])
+const dimReduction = ref()
 </script>
 
 <template>
@@ -11,9 +18,13 @@ const useDGrid = ref(true)
 
     <div class="menu-container">
 
-      <div class="toggle-switch-container">
+      <div class="toggle-switch">
         <label for="d-grid-toggle">DGrid</label>
         <ToggleSwitch v-model="useDGrid" inputId="d-grid-toggle" />
+      </div>
+
+      <div>
+        <SelectButton v-model="dimReduction" :options="dimReductionOptions" option-label="name" />
       </div>
 
     </div>
@@ -30,12 +41,19 @@ const useDGrid = ref(true)
 }
 
 .menu-container {
-  display: inline-block;
-  padding: 1.5rem;
-  border-right: 3px solid lightgrey;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.75rem;
+  margin-bottom: 0.75rem;
+  border-right: 2px solid black;
 }
 
-.toggle-switch-container {
+.menu-container > * {
+  margin: 0.75rem 1.5rem;
+}
+
+.toggle-switch {
   display: flex;
   align-items: center;
   gap: 0.5rem;
