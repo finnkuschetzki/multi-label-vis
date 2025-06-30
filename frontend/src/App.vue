6b018662252fa6eedd9c8a72d2322ec2fc17fabb
Finn Kuschetzki <finnmail@icloud.com>
2025-06-30 19:38:51 +0200

    implemented tool menu prototype

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,40 @@
 <script setup>
+import { ref } from "vue"
 
+// menu refs
+const useDGrid = ref(true)
 </script>
 
 <template>
+  <div class="main-container">
 
+    <div class="menu-container">
+
+      <div class="toggle-switch-container">
+        <label for="d-grid-toggle">DGrid</label>
+        <ToggleSwitch v-model="useDGrid" inputId="d-grid-toggle" />
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
+.main-container {
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.menu-container {
+  display: inline-block;
+  padding: 1.5rem;
+  border-right: 3px solid lightgrey;
+}
+
+.toggle-switch-container {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
 </style>
