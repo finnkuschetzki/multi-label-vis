ea5f7ba98b9a83dc4cc3529890d79dfa7d3a9eae
Finn Kuschetzki <finnmail@icloud.com>
2025-07-14 19:27:57 +0200

    implemented segment fill (0/1 fill)

--- frontend/src/chart/base.js
+++ frontend/src/chart/base.js
@@ -68,10 +68,10 @@ export function updateChart() {
         (yScale(0.01) - yScale(0))
     ) * 0.9  // todo scale size to prevent overlaps (caused by stroke width)
 
-    // calculating vertices
-    const vertices = []
+    // calculating glyphData
+    const glyphData = []
 
-    const initialRadians = Math.PI / 2
+    const initialRadians = 3 / 2 * Math.PI
     const classStep = (2 * Math.PI) / numClasses
 
     data.value.forEach(d => {
@@ -89,41 +89,79 @@ export function updateChart() {
             ])
         }
 
-        vertices.push({
+        const segments = []
+        for (let i = 0; i < circlePoints.length; i++) {
+            segments.push({
+                "classIndex": i,
+                "centerPoint": [mx, my],
+                "outerPoints": [circlePoints[i], circlePoints[(i+1) % numClasses]],
+                "groundTruth": d["ground_truth"][i],
+                "prediction": d["predictions"][i],
+                "binarizedPrediction": d["binarized_predictions"][i]
+            })
+        }
+
+        glyphData.push({
             "mx": mx,
             "my": my,
-            "circlePoints": circlePoints
+            "circlePoints": circlePoints,
+            "segments": segments
         })
     })
 
-    // removing old glyphs
-    contentGroup.selectAll(".base-glyph").remove()
+    const segmentData = glyphData.flatMap(glyph => glyph.segments)
 
-    // base glyph (outline and segment borders)
-    contentGroup.selectAll(".base-glyph")
-        .data(vertices)
+    // removing old glyphs
+    contentGroup.selectAll(".glyph-lines").remove()
+    contentGroup.selectAll(".glyph-segment-fills").remove()
+
+    // segment fills
+    contentGroup.selectAll(".glyph-segment-fills")
+        .data(segmentData)
         .enter()
         .append("path")
-        .attr("class", "base-glyph")
-        .attr("d", v => {
-            // outline
-            let d = `M${v.circlePoints[0][0]},${v.circlePoints[0][1]}`  // move to first point
-            for (let i = 1; i < numClasses; i++) {
-                d += `L${v.circlePoints[i][0]},${v.circlePoints[i][1]}`  // lines to other points
+        .attr("class", "glyph-segment-fills")
+        .attr("d", s => {
+            let d = ``
+
+            // segment fills
+            for (let i = 0; i < numClasses; i++) {
+                d += `M${s.centerPoint[0]},${s.centerPoint[1]}`
+                d += `L${s.outerPoints[0][0]},${s.outerPoints[0][1]}`
+                d += `L${s.outerPoints[1][0]},${s.outerPoints[1][1]}`
+                d += `Z`
             }
-            d += `Z`  // complete to first point
+
+            return d
+        })
+        .attr("stroke", "none")
+        .attr("fill", s => s.binarizedPrediction ? tableau20[s.classIndex] : "none")
+
+    // glyph lines (outline and segment borders)
+    contentGroup.selectAll(".glyph-lines")
+        .data(glyphData)
+        .enter()
+        .append("path")
+        .attr("class", "glyph-lines")
+        .attr("d", g => {
+            let d = ``
 
             // segment borders
             for (let i = 0; i < numClasses; i++) {
-                d += `M${v.mx},${v.my}`
-                d += `L${v.circlePoints[i][0]},${v.circlePoints[i][1]}`
+                d += `M${g.mx},${g.my}`
+                d += `L${g.circlePoints[i][0]},${g.circlePoints[i][1]}`
             }
+
+            // outline
+            d += `M${g.circlePoints[0][0]},${g.circlePoints[0][1]}`  // move to first point
+            for (let i = 1; i < numClasses; i++) {
+                d += `L${g.circlePoints[i][0]},${g.circlePoints[i][1]}`  // lines to other points
+            }
+            d += `Z`  // complete to first point
 
             return d
         })
         .attr("stroke", "black")
         .attr("stroke-width", .1)
         .attr("fill", "none")
-
-
 }
