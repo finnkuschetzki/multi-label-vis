a89ec1100df2f6dda1af3138ba04f5ebc35ae4a4
Finn Kuschetzki <finnmail@icloud.com>
2025-09-11 21:52:10 +0200

    disabled vue dev tools

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
     Components({
       resolvers: [
           PrimeVueResolver()
